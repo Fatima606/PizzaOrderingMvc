7f18842147859e9e12e2d02724269e74e3eecae0
fatima.zafar <fatima.zafar@TS-WKS-44>
2024-07-23 15:42:41 +0500

    PizzaController

--- PizzaOrderingMvc/Program.cs
+++ PizzaOrderingMvc/Program.cs
@@ -31,6 +31,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Pizza}/{action=CreatePizza}/{id?}");
 
 app.Run();
